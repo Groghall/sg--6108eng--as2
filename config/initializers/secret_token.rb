d80c314304db7e08db1ce01ecb95a07de56b633f
Stephen Gault <s.gault@2009.ljmu.ac.uk>
2013-02-08 12:13:50 +0000

    AS2 for 6108eng

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Welcome::Application.config.secret_token = '188085e350743032e2655bc8cca6b25361239e0fa4d9907cbce4e6c19f42891646302977c688a1d21969d775e30c289b22463204bad5a8e1572ea05a8d58c8c8'
---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Welcome::Application.routes.draw do
+  resources :events
+  
+  get 'welcome/index'
+
+  get 'welcome/about'
+
+  get 'welcome/contact'
+
+  get 'welcome/team'
+
+  get 'welcome/sessions'
+  
+end
